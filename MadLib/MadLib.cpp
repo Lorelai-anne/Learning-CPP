e2f18915b98132fc578cc0557f8ae6a15cbd961f
Lorelai-anne <lorelai.anne03@gmail.com>
2025-09-21 16:42:03 -0400

    added helper method in madLib.cpp

--- MadLib/MadLib.cpp
+++ MadLib/MadLib.cpp
@@ -1,23 +1,34 @@
 // MadLibs.cpp
-// 
+// Creates a short six lie story and asks the user for ~8 different words
+// contains a main method and a helper method ask(string prompt) to fix repetition
 //
 
 #include <iostream>
 #include <string>
-#include <cstdio>
+
+std::string ask(std::string prompt) {
+	std::string response;
+	std::cout << prompt;
+	while (response.empty()) {
+		std::getline(std::cin, response);
+	}
+	return response;
+}
 
 int main() {
-	std::string adjective = "happy";
-	std::string noun = "spoon";
-	std::string adverb = "running";
-	std::string animal = "hippo";
-	std::string verb = "run";
-	std::string color = "purple";
-	std::string pluralNoun = "spoons";
+	std::string adjective = ask("Enter a adjective: ");
+	std::string noun = ask("Enter a noun: ");
+	std::string adverb = ask("Enter an adverb: ");
+	std::string animal = ask("Enter an animal: ");
+	std::string verb = ask("Enter a verb: ");
+	std::string color = ask("Enter a color: ");
+	std::string pluralNoun = ask("Enter a plural noun: ");
+	std::string celebrity = ask("Enter a celebrity: ");
 
 	std::cout << "This morning I grabbed my " << adjective << " backpack and ran to the " << noun << ".\n";
 	std::cout << "On my way, a " << color << " " << animal << " jumped out and tried to " << verb << " my " << noun << "!\n";
 	std::cout << "I shouted " << adjective << " words and chased it " << adverb << " down the street.\n";
-	std::cout << "Then I tripped over a " << adjective << " " << noun << " and landedd right on a pile of " << pluralNoun << ".\n";
+	std::cout << "Then I tripped over a " << adjective << " " << noun << " and landed right on a pile of " << pluralNoun << ".\n";
 	std::cout << "Everyone around started to " << verb << " while pointing at my " << color << " shoes.\n";
+	std::cout << "Thankfully, as I was sprawled across the floor " << celebrity << " came to my rescue and we ran into the sunset together\n";
 }
